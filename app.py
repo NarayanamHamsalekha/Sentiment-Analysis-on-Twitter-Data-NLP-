472c72cb3393b124a0090f7648b59c8c0cf5a5bb
NarayanamHamsalekha <nhamsalekhahamsa@gmail.com>
2026-04-08 01:02:13 +0530

    Simplify app.run_server call
    Remove explicit port setting from app.run_server.

--- app.py
+++ app.py
@@ -107,5 +107,6 @@ def predict(n, text):
     return ""
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8050))
-    app.run_server(host='0.0.0.0', port=port, debug=False)
+    app.run_server(debug=False)
+
+
