aaa452c4eb7e8fa0197d0ddfb02c798633037184
NarayanamHamsalekha <nhamsalekhahamsa@gmail.com>
2026-04-08 00:41:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,4 +106,5 @@ def predict(n, text):
     return ""
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(host='0.0.0.0', port=port, debug=False)
